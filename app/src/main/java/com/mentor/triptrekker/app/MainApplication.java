94a9fd37c53e38beab47df6bd91c4c929432a48c
Hassan Askar <hassanemad15697@gmail.com>
2024-04-23 23:17:19 +0200

    refactoring

--- app/src/main/java/com/mentor/triptrekker/app/MainApplication.java
+++ app/src/main/java/com/mentor/triptrekker/app/MainApplication.java
@@ -2,10 +2,12 @@ package com.mentor.triptrekker.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.mentor.triptrekker.*"})
+@EntityScan(basePackages = {"com.mentor.triptrekker.*"})
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
--- booking-service/src/main/java/com/mentor/triptrekker/booking/service/BookingService.java
+++ booking-service/src/main/java/com/mentor/triptrekker/booking/service/BookingService.java
@@ -15,7 +15,25 @@ public class BookingService {
     }
 
     private void processBooking(FlightBookingData bookingData) {
-        System.out.println("booking data received: "+bookingData.getTravelersData().get(0).getFirstName());
+        // process the booking with unconfirmed status
+
+
+        // publish the payment data and order it to the payment service to complete the payment
+        System.out.println("booking data received with name : "+bookingData.getTravelersData().get(0).getFirstName());
+        System.out.println("booking data received with card number : "+bookingData.getPaymentData().getCardNumber());
         // Booking logic here
     }
+
+    private void confirmBooking(Integer bookingId){
+        // update the booking status to be confirmed
+    }
+
+
+    private void notify(String message){
+
+        // publish message to notification service
+    }
+
+
+
 }
